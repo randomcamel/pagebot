23af61203a27f27df3103d2a0340500aa8360f70
Chris Doherty <cdoherty@ooyala.com>
2014-10-02 16:10:29 -0700

    Bot to listen on IRC and send alerts to Hipchat.

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+gem "cinch"
+gem "circular_queue"
+gem "hipchat"
+gem "summer"
--- pagebot.rb
+++ pagebot.rb
@@ -2,23 +2,55 @@
 
 require 'cinch'
 require 'circular_queue'
+require 'hipchat'
+require 'summer'
 
-$buffers = {}
+require 'pry'
 
-bot = Cinch::Bot.new do
-  configure do |c|
-    c.server = "irc.freenode.org"
-    c.channels = ["#cinch-bots"]
-    c.nick = "hjkl"
-  end
+# hipchat key.
+require './secrets'
 
-  # on :message do |m|
-  #   $buffers[nick] 
-  # end
+# eventually it'd be nice to have surrounding context.
+class Rings
+  LINE_BUFFER_SIZE = 3
 
-  on :message, /!PRIVMSG (.+?) (.+)/ do |m, nick, message|
-    m.reply "Hello, #{m.inspect}"
+  class <<self
+    @rings = {}
+
+    def [](nick)
+      @rings[nick] ||= CircularQueue.new(LINE_BUFFER_SIZE)
+    end
   end
 end
 
-bot.start
+# [3] pry(main)> room = c.create_room("IRC Integration")
+# => {"id"=>864742, "links"=>{"self"=>"https://api.hipchat.com/v2/room/864742"}}
+
+class Bot < Summer::Connection
+  HIPCHAT_ROOM = "IRC 2"
+
+  def initialize(*args)
+    @hipchat = HipChat::Client.new(HIPCHAT_API_KEY, :api_version => 'v2')
+    super(*args)
+  end
+
+  def channel_message(sender, channel, message)
+    if message =~ /windows/i
+      privmsg("oink", channel)
+      binding.pry
+      msg = <<-EOS
+IRC alert for @cdoherty:
+
+#{channel}: <#{sender[:nick]}> #{message}
+EOS
+      @hipchat[HIPCHAT_ROOM].send("cdoherty", msg, :color => "purple", :message_format => "text")
+    end
+  end
+end
+
+if ARGV.size == 0
+  Bot.new("localhost")
+else
+  @c = HipChat::Client.new(HIPCHAT_API_KEY, :api_version => 'v2')
+  binding.pry
+end
